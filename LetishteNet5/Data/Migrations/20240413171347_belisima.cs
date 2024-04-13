704b7f794fd7f1731f6fa69cba4f222cb6e465cc
Kolyo1 <dimovnikola705@gmail.com>
2024-04-13 20:19:13 +0300

    dropdown menu  for reservation tickettype done

---
+++ LetishteNet5/Data/Migrations/20240413171347_belisima.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LetishteNet5.Data.Migrations
+{
+    public partial class belisima : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
