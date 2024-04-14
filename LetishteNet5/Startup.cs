0e6ff13e77f5657db7bbc9b002fa7533cec92ebf
Kolyo1 <dimovnikola705@gmail.com>
2024-04-14 05:41:40 +0300

    m

--- LetishteNet5/Startup.cs
+++ LetishteNet5/Startup.cs
@@ -26,22 +26,7 @@ namespace LetishteNet5
 
         public IConfiguration Configuration { get; }
 
-        /*
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<FmDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<FmDbContext>();
-            services.AddControllersWithViews();
-        }
-        */
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
@@ -52,7 +37,7 @@ namespace LetishteNet5
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
