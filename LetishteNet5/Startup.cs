8c002db6c384804c2459c60036b5ef315e9ca8a8
Kolyo1 <dimovnikola705@gmail.com>
2024-04-14 12:17:11 +0300

    Revert "kom"
    This reverts commit 6cff4bb9fa2f32c9054a0868c1464223471f8af3.

--- LetishteNet5/Startup.cs
+++ LetishteNet5/Startup.cs
@@ -1,4 +1,3 @@
-
 using LetishteNet5.Data;
 using LetishteNet5.Data.Entities;
 using Microsoft.AspNetCore.Builder;
@@ -26,118 +25,106 @@ namespace LetishteNet5
 
         public IConfiguration Configuration { get; }
 
-        /*
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<FmDbContext>(options =>
+            services.AddDbContext<FlightManagerDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<FmDbContext>();
-            services.AddControllersWithViews();
-        }
-        */
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
-        {
-            if (env.IsDevelopment())
+            services.AddDefaultIdentity<User>(options =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            CreateRole(serviceProvider).Wait();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<FlightManagerDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddIdentity<User, IdentityRole>(options =>
-            {
-                options.SignIn.RequireConfirmedAccount = false;
-
+                options.SignIn.RequireConfirmedAccount = true;
                 options.Password.RequireDigit = false;
-                options.Password.RequiredLength = 3;
-                options.Password.RequireLowercase = false;
                 options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 3;
                 options.Password.RequiredUniqueChars = 0;
-
-            })
+                options.User.RequireUniqueEmail = true;
+            }).AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<FlightManagerDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+
+            /*     services.AddTransient<IFlightService, FlightService>();
+                 services.AddTransient<IReservationService, ReservationService>();
+                 services.AddTransient<IUserService, UserService>();*/
         }
 
-        private async Task CreateRole(IServiceProvider serviceProvider)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var UserManager = serviceProvider.GetRequiredService<UserManager<User>>();
-            string[] roles = { "Admin", "Employee" };
-            IdentityResult result;
-
-            //Checks if roles Admin and Employee exist. If not - they get created
-            foreach (var role in roles)
+            using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                var check = await RoleManager.RoleExistsAsync(role);
-                if (!check)
+                using (var context = serviceScope.ServiceProvider.GetRequiredService<FlightManagerDbContext>())
                 {
-                    result = await RoleManager.CreateAsync(new IdentityRole(role));
+                    context.Database.EnsureCreated();
+
+                    if (!context.Roles.Any())
+                    {
+                        context.Roles.Add(new IdentityRole
+                        {
+                            Name = "Admin",
+                            NormalizedName = "ADMIN"
+                        });
+
+                        User admin = new User();
+                        admin.UserName = "admin@abv.bg";
+                        admin.FirstName = "admin";
+                        admin.Email = "admin@abv.bg";
+                        var hasher = new PasswordHasher<User>();
+
+                        admin.PasswordHash = hasher.HashPassword(admin,"12345678");
+                        var adminRole = context.Roles.First(x=>x.Name == "Admin");
+                        var userRole = new IdentityUserRole<string>();
+                        userRole.UserId = admin.Id;
+                        userRole.RoleId = adminRole.Id;
+                        context.Users.Add(admin);
+                        context.UserRoles.Add(userRole);
+                        context.SaveChanges();
+
+
+
+                        context.Roles.Add(new IdentityRole
+                        {
+                            Name = "User",
+                            NormalizedName = "USER"
+                        });
+
+                        context.SaveChanges();
+                    }
                 }
-            }
-            // Add admin user
-            var admin = new User
-            {
-                UserName = "Admin@admin.bg",
-                FirstName = "Admin",
-                LastName = "Admin",
-                EGN = "0000000000",
-                Address = "AdminNoAddress123",
-                Email = "Admin@admin.bg",
-                PhoneNumber = "1234567890",
-                Id = Guid.NewGuid().ToString()
-            };
 
-            string passwordUser = "_Password123";
 
-            //Checks if admin user exists. If it doesn't, it creates a admin user with role ,,Admin''
-            var _user = await UserManager.FindByNameAsync(admin.UserName);
-            if (_user == null)
-            {
-                IdentityResult checkUser = await UserManager.CreateAsync(admin, passwordUser);
-                if (checkUser.Succeeded)
+
+                if (env.IsDevelopment())
                 {
-                    await UserManager.AddToRoleAsync(admin, "Admin");
+                    app.UseDeveloperExceptionPage();
+                    app.UseMigrationsEndPoint();
                 }
-            }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
 
+                app.UseRouting();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
+                    endpoints.MapRazorPages();
+                });
+            }
         }
     }
 }
